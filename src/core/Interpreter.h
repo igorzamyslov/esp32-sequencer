876b254c6771feec64d2e0f851caed5786dd2ffa
Igor Zamyslov <zamylovigor@gmail.com>
2026-05-04 20:57:24 +0200

    feat(core): recursive interpreter for sequences

---
+++ src/core/Interpreter.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Block.h"
+#include "Sequence.h"
+namespace seqb {
+class Registry;
+class Interpreter {
+public:
+    explicit Interpreter(Registry& r) : reg_(r) {}
+    RunResult runSequence(const Sequence& s, RunCtx& ctx);
+    RunResult runNode(const Node& n, RunCtx& ctx);
+    RunResult runSlot(const std::vector<Node>& nodes, RunCtx& ctx);
+private:
+    Registry& reg_;
+};
+}
