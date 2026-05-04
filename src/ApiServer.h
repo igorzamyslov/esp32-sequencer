ea5ab0cc23b6db936a166e26057686698723e813
Igor Zamyslov <zamylovigor@gmail.com>
2026-05-04 21:10:52 +0200

    feat: JSON API server for schema, sequences, triggers, status

---
+++ src/ApiServer.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <ESPAsyncWebServer.h>
+#include "core/SequenceStore.h"
+#include "core/TriggerStore.h"
+#include "core/TriggerManager.h"
+#include "core/Registry.h"
+#include <functional>
+
+namespace seqb {
+
+struct ApiHooks {
+    std::function<void(const std::string& sequenceId)> enqueueRun;
+    std::function<std::string()> currentStatusJson;
+};
+
+class ApiServer {
+public:
+    ApiServer(AsyncWebServer& srv,
+              SequenceStore& seqs,
+              TriggerStore&  trigs,
+              TriggerManager& tm,
+              ApiHooks hooks);
+    void registerRoutes();
+private:
+    AsyncWebServer& srv_;
+    SequenceStore&  seqs_;
+    TriggerStore&   trigs_;
+    TriggerManager& tm_;
+    ApiHooks        hooks_;
+};
+
+}
